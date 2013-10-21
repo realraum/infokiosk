79f87af36d9a5d3ea62462f6be1be04d8fc60a73
Bernhard Tittelbach <xro@realraum.at>
2013-10-21 07:42:22 +0000

    resolutions

--- start-kiosk.sh
+++ start-kiosk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-KIOSKURI_DEFAULT="http://www.realraum.at/kiosk.html"
-KIOSKURI_900x1440="http://www.realraum.at/kiosk.html"
+KIOSKURI_DEFAULT="http://www.realraum.at/kiosk900x1440.html"
+KIOSKURI_900x1440="http://www.realraum.at/kiosk900x1440.html"
 KIOSKURI_768x1366="http://www.realraum.at/kiosk768x1366.html"
-KIOSKURI_1024x768="http://www.realraum.at/kiosk2cal.html"
+KIOSKURI_1024x768="http://www.realraum.at/kiosk1024x768.html"
 #KIOSKURI="http://grical.realraum.at"
 #KIOSKURI="http://radi-o-matic.helsinki.at/nextshows/index.php?len=11"
 sleep 1
@@ -21,7 +21,7 @@ xset s noblank
 RESOLUTION=$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");print $2}')
 KIOSKURI=$(eval echo -n \${KIOSKURI_${RESOLUTION}:-$KIOSKURI_DEFAULT})
 unclutter &
-#midori -e Fullscreen -a "$KIOSKURI"
-chromium --kiosk --incognito "$KIOSKURI"
+midori -e Fullscreen -a "$KIOSKURI"
+#chromium --kiosk --incognito "$KIOSKURI"
 
 exit 0
