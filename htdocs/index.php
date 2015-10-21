4a56f51568c7404c4b78e6928d5bdafc25dce1a1
verr <dev@2904.cc>
2015-10-21 04:55:35 +0200

    added redirect to https blog

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,11 @@
 <?
-	Header('Location:http://wp.realraum.at/');
+
+// this is a quick "hack" 
+// to be used until xro enables proper redirect via apache2
+
+    if(empty($_SERVER['HTTPS'])) {
+        Header('Location:http://wp.realraum.at/');
+    } else {
+        Header('Location:https://wp.realraum.at/');
+    }
 ?>
