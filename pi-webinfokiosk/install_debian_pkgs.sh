53606d1a506ca344e4af451db7ce4b3fb984389f
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-12-12 05:57:15 +0100

    finally fix user xinit

--- pi-webinfokiosk/install_debian_pkgs.sh
+++ pi-webinfokiosk/install_debian_pkgs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo mount / -o remount,rw
 sudo loginctl enable-linger pi
+mkdir -p .config/epiphany
 sudo apt-get update
-sudo apt-get install --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd xinit
+sudo apt-get install --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd xinit xserver-xorg x11-xserver-utils xserver-xorg-video-fbturbo
+sudo chmod +s /usr/lib/xorg/Xorg /usr/bin/X
 systemctl --user enable startx.service
 systemctl --user enable restartx.timer
 systemctl --user start restartx.timer startx.service
