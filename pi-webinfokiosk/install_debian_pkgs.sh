87358a2048e253b56a96bad98bd2eb5a68431837
Bernhard Tittelbach <bernhard@tittelbach.org>
2016-12-12 05:27:20 +0100

    raspi0_config_stuff

--- pi-webinfokiosk/install_debian_pkgs.sh
+++ pi-webinfokiosk/install_debian_pkgs.sh
@@ -2,7 +2,7 @@
 sudo mount / -o remount,rw
 sudo loginctl enable-linger pi
 sudo apt-get update
-sudo apt-get install --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd
+sudo apt-get install --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd xinit
 systemctl --user enable startx.service
 systemctl --user enable restartx.timer
 systemctl --user start restartx.timer startx.service
