1ca33d670d4fed3a3ecbf439c36f983ab78a136a
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-04-09 15:15:00 +0200

    wegmitswap

--- pi-webinfokiosk/install_debian_pkgs.sh
+++ pi-webinfokiosk/install_debian_pkgs.sh
@@ -4,6 +4,7 @@ sudo loginctl enable-linger pi
 mkdir -p .config/epiphany
 sudo apt-get update
 sudo apt-get install --no-install-recommends spectrwm unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd xinit xserver-xorg x11-xserver-utils xserver-xorg-video-fbturbo matchbox-window-manager
+sudo apt-get purge dphys-swapfile
 sudo chmod +s /usr/lib/xorg/Xorg /usr/bin/X
 systemctl --user enable startx.service
 systemctl --user enable restartx.timer
