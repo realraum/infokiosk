03ea1ffc816860c0fcd04b3000520f9593972eab
Bernhard Tittelbach <bernhard@tittelbach.org>
2017-10-15 22:47:22 +0200

    fix

--- htdocs/kiosk.js
+++ htdocs/kiosk.js
@@ -310,7 +310,7 @@ function writeAnwesenheitStatus(data)
     if (data.sensors.barometer)
     {
       sensorsdiv+='<div class="sensorstatus"><b><u>Barometer</u></b>';
-      $r3jq.each( data.sensors.temperature, function(s, sensorobj) {
+      $r3jq.each( data.sensors.barometer, function(s, sensorobj) {
         sensorsdiv+='<br/>'+sensorobj.location+': '+sensorobj.value.toFixed(2)+sensorobj.unit;
         drawGauge($r3jq('.barometergauge[sensorlocation=\''+sensorobj.location+'\']').get()[0], "Barometer "+sensorobj.location, sensorobj.value, {redFrom: 950, redTo: 1024,yellowFrom:0, yellowTo: 200,minorTicks: 4, min:0, max:1024});
       });
