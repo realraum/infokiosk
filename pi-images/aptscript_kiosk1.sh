b02befcf1bcbdf972b3530bc8a747f84741497fe
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-04-28 17:00:15 +0200

    --bug

--- pi-images/aptscript_kiosk1.sh
+++ pi-images/aptscript_kiosk1.sh
@@ -3,6 +3,6 @@ apt-get update
 apt-get purge --yes dphys-swapfile avahi-daemon
 apt autoremove
 apt-get upgrade --yes
-apt-get install --yes --no-install-recommends zsh tmux vim libpam-dbus libpam-systemd rsync inotify-tools pypy fbi samba inotify-tools libimage-exiftool-perl mosquitto mosquitto-clients bluetooth bluez-obex bluez-tools libbluetooth-dev 
+apt-get install --yes --no-install-recommends zsh tmux vim libpam-dbus libpam-systemd rsync inotify-tools pypy fbi samba inotify-tools libimage-exiftool-perl mosquitto mosquitto-clients bluetooth bluez-obexd bluez-tools libbluetooth-dev
 /bin/loginctl enable-linger pi
 
