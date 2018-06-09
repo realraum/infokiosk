502d2a15f4cf4745186f33c12ba04cf64b9bf907
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-06-09 21:14:21 +0200

    remove translate this page

--- pi-images/rootfs-kiosk2/usr/local/bin/start-kiosk.sh
+++ pi-images/rootfs-kiosk2/usr/local/bin/start-kiosk.sh
@@ -24,10 +24,16 @@ RESOLUTION=$(xrandr -q | awk -F'current' -F',' 'NR==1 {gsub("( |current)","");pr
 KIOSKURI=$(eval echo -n \${KIOSKURI_${RESOLUTION}:-$KIOSKURI_DEFAULT})
 spectrwm &
 unclutter &
+rm -Rf /run/user/1000/browser
 mkdir -p /run/user/1000/browser
 mkdir -p /run/user/1000/browsercache
 #epiphany --profile /run/user/1000/browser  -a "$KIOSKURI"
 #midori -e Fullscreen -a "$KIOSKURI"
+cat > /run/user/1000/browser/Preferences <<EOF
+{"first_run": false,"translate":{"enabled":false},"translate_blocked_languages":["de","en"]}
+EOF
+chmod 600 /run/user/1000/browser/Preferences
+#export LANG=de_AT.UTF-8
 chromium-browser --kiosk --incognito --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --disk-cache-dir=/run/user/1000/browsercache --user-data-dir=/run/user/1000/browser --start-maximized    "$KIOSKURI"
 exit 0
 
