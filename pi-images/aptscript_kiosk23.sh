6e0480ffc1b8c490bca07e9ec3535b0fc351febf
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-08-28 02:28:51 +0200

    make sure to linger

--- pi-images/aptscript_kiosk23.sh
+++ pi-images/aptscript_kiosk23.sh
@@ -5,6 +5,6 @@ apt autoremove
 apt-get upgrade --yes
 apt-get install --yes --no-install-recommends xserver-xorg accountsservice xserver-xorg-video-fbturbo xserver-xorg-input-evdev xinput-calibrator matchbox-keyboard xinput xinit spectrwm x11-xserver-utils unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd rsync matchbox-window-manager rpi-chromium-mods
 sudo chmod +s /usr/lib/xorg/Xorg /usr/bin/X
-/bin/systemctl enable ssh.service getty@ttyGS0.service 
+/bin/systemctl enable ssh.service getty@ttyGS0.service
 /bin/loginctl enable-linger pi
 
