f1e62a06090bbbd132a169c2530b276eb7bbc3b7
Bernhard Tittelbach <bernhard@tittelbach.org>
2018-08-28 02:31:29 +0200

    autoremove last

--- pi-images/aptscript_kiosk23.sh
+++ pi-images/aptscript_kiosk23.sh
@@ -1,10 +1,10 @@
 DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get purge --yes dphys-swapfile avahi-daemon
-apt autoremove
 apt-get upgrade --yes
 apt-get install --yes --no-install-recommends xserver-xorg accountsservice xserver-xorg-video-fbturbo xserver-xorg-input-evdev xinput-calibrator matchbox-keyboard xinput xinit spectrwm x11-xserver-utils unclutter epiphany-browser zsh tmux vim libpam-dbus libpam-systemd rsync matchbox-window-manager rpi-chromium-mods
 sudo chmod +s /usr/lib/xorg/Xorg /usr/bin/X
 /bin/systemctl enable ssh.service getty@ttyGS0.service
 /bin/loginctl enable-linger pi
+apt autoremove
 
