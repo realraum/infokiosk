e3572ca1121aa00209e65a3d60b6be3626bfd5ce
Bernhard Tittelbach <bernhard@tittelbach.org>
2019-09-18 09:20:44 +0800

    change stuff

--- pi-images/aptscript_kiosk1.sh
+++ pi-images/aptscript_kiosk1.sh
@@ -5,4 +5,4 @@ apt autoremove
 apt-get upgrade --yes
 apt-get install --yes --no-install-recommends zsh tmux vim libpam-dbus libpam-systemd rsync inotify-tools pypy fbi samba inotify-tools libimage-exiftool-perl mosquitto mosquitto-clients bluetooth bluez-obexd bluez-tools libbluetooth-dev
 /bin/loginctl enable-linger pi
-
+apt-get purge rsyslog
