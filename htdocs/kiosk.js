05d38c5593f3cc4f22a726e0c392e59e19400c85
Bernhard Tittelbach <bernhard@tittelbach.org>
2025-11-09 06:16:47 +0100

    switch to new URL

--- htdocs/kiosk.js
+++ htdocs/kiosk.js
@@ -442,9 +442,9 @@ function writeAnwesenheitStatus(data)
 function updateAnwesenheitStatus()
 {
  // Future URL
- //url = "//status.realraum.at/spaceapi.json";
+ url = "//status.realraum.at/spaceapi.json";
  // Old URL
- url = "//realraum.at/status.json";
+ //url = "//realraum.at/status.json";
  // Test URL
  // url = "/status.json";
  var jqxhr = $r3jq.getJSON(url, writeAnwesenheitStatus);
